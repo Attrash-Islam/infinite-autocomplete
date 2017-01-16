c06ffe6c2e741cae9eb787133146ad8587f7c502
Islam Attrash <iattrash@vasonanetworks.com>
2017-01-16 23:47:51 +0200

    InfiniteAutocompleteConfig interface documentation

--- src/Interfaces/InfiniteAutocompleteConfig.d.ts
+++ src/Interfaces/InfiniteAutocompleteConfig.d.ts
@@ -5,9 +5,24 @@ import { IOptionObject } from './IOption';
  * @author Islam Attrash
  */
 export interface InfiniteAutocompleteConfig {
+    /**
+     * data static source
+     */
     data?:Array<any>;
+    /**
+     * on-select event output handler when choosing an option
+     */
     onSelect?:Function;
+    /**
+     * fetching x of data for every scroll bottom (or new input)
+     */
     fetchSize?:number;
+    /**
+     * max height for the results options
+     */
     maxHeight?:string;
+    /**
+     * data dynamic api source
+     */
     getDataFromApi?(text:string, page:number, fetchSize:number):Promise<Array<any>>;
 }
