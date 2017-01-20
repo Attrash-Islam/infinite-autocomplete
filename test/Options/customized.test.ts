f02568b1f109747527b3391f86ceeebcf4e3a1e6
Islam Attrash <isattrash@gmail.com>
2017-01-20 22:46:21 +0200

    Removed depracted test

--- test/Options/customized.test.ts
+++ test/Options/customized.test.ts
@@ -3,51 +3,6 @@ import { TestUtils } from '../Utils/index';
 import { Promise as es6Promise } from 'es6-promise';
 import { template } from './Customization/template';
 
-describe(`Customized Option object implementation: `, function() {
-
-    it(`should fetch text and value based on my IOptionObject implementation logic`, 
-        async function(done):es6Promise<any> {
-            var infinite = document.createElement('div');
-            var iniElm = new InfiniteAutocomplete(infinite);
-            iniElm.setConfig({data: [
-                    { text: 'first', value: 1},
-                    { text: 'second', value: 2},
-                    { text: 'theird', value: 3},
-                    { text: 'fourth', value: 4},
-                    { text: 'fivth', value: 5}
-                ]});
-
-                var input = <HTMLInputElement> infinite.querySelector(`input`);
-                TestUtils.typeLetter(input, 'f');
-                await TestUtils.sleep(0);
-                var options = <NodeListOf<HTMLElement>> infinite.querySelectorAll(`li`);
-                if(options.length === 0) {
-                    throw `options shouldn't be empty!`;
-                }
-                for( var i = 0; i < options.length; i++) {
-                    expect(options[i].innerText)
-                        .toContain('f');
-                }
-
-                var optionsList = <HTMLElement> infinite.querySelector(`ul`);
-                expect(optionsList.style.display)
-                    .toBe(``);
-
-                TestUtils.typeLetter(input, 'i');
-                await TestUtils.sleep(0);
-                var options = <NodeListOf<HTMLElement>> infinite.querySelectorAll(`li`);
-                if(options.length === 0) {
-                    throw `options shouldn't be empty!`;
-                }
-                for( var i = 0; i < options.length; i++) {
-                    expect(options[i].innerText)
-                        .toContain('fi');
-                }
-                done();
-    });
-
-});
-
 describe(`Customized options template: `, function() {
 
     it(`The rendered options template must be replaced 
