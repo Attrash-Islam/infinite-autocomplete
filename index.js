f96197363850b3fda8bfec8c21b88f15ceb750f2
Islam Attrash <isattrash@gmail.com>
2018-02-21 13:49:07 +0200

    [bugfix] ES5 not compatible entry point (index.js)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require(`./dist/index.js`);
+  module.exports = require("./dist/index.js");
 } else {
-  module.exports = require(`./dist/index.debug.js`);
+  module.exports = require("./dist/index.debug.js");
 }
